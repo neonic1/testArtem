617927be2fb33ee2b26b2cd17ac45f56be54a08d
Artem <37310827+neonic1@users.noreply.github.com>
2024-06-30 19:55:23 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ const CLIENT_SECRET = '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';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 const REFRESH_TOKEN = '1//04AHjwghZSZAYCgYIARAAGAQSNwF-L9IrdSbWBhLiXBmKn84LfD7rWjqzHE8OArQ_cYniSLf4qzCGPNh7wf8HOmWM3XZJ7ejsrf8';
 
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+const CLIENT_ID_2 = '352377907218-d1ctco2o50a10v45hdeh5v6rakdfs0fb.apps.googleusercontent.com';
+const CLIENT_SECRET_2 = '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';
+const REDIRECT_URI_2 = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN_2 = '1//04fpNzRnUqr_LCgYIARAAGAQSNwF-L9Ir8Dlf19R_Ht9d7-FpApeV6JHKJAtDOssdEFVQcVboRGPp9LCEk0uc2Yromcfr55zvD9c';
 
 async function sendMail(from, to, subject, text) {
-  try {
-    const accessToken = await oAuth2Client.getAccessToken();
+  let oAuth2Client, transportOptions;
 
-    const transport = nodemailer.createTransport({
+  if (from === 'paul@martlet-express.com') {
+    oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+    transportOptions = {
       service: 'gmail',
       auth: {
         type: 'OAuth2',
@@ -26,15 +30,33 @@ async function sendMail(from, to, subject, text) {
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
-        accessToken: accessToken.token,
+        accessToken: (await oAuth2Client.getAccessToken()).token,
       },
-    });
+    };
+  } else {
+    oAuth2Client = new google.auth.OAuth2(CLIENT_ID_2, CLIENT_SECRET_2, REDIRECT_URI_2);
+    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN_2 });
+    transportOptions = {
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: from,  // Change this to from
+        clientId: CLIENT_ID_2,
+        clientSecret: CLIENT_SECRET_2,
+        refreshToken: REFRESH_TOKEN_2,
+        accessToken: (await oAuth2Client.getAccessToken()).token,
+      },
+    };
+  }
+
+  try {
+    const transport = nodemailer.createTransport(transportOptions);
 
     const mailOptions = {
       from: from,
       to: to,
       subject: subject,
-      text: text
+      text: text,
     };
 
     const result = await transport.sendMail(mailOptions);
